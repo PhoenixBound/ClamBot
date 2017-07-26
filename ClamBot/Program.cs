3f74c1216428454396815d49bc22c32d02ffb085
PhoenixBound <phoenixbound.github@gmail.com>
2017-07-26 13:24:03 -0500

    Get the bot to connect Note: requires a separate "ClientDetails.txt" containing the OAuth token. This won't be provided.

--- ClamBot/Program.cs
+++ ClamBot/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ClamBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            string token = await ReadToken();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task<string> ReadToken()
+        {
+            string fileToken;
+
+            using (StreamReader sr = new StreamReader(File.OpenRead("ClientDetails.txt")))
+            {
+                fileToken = await sr.ReadLineAsync();
+            }
+
+            return fileToken;
         }
     }
 }
